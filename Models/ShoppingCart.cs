61aacf44fcfb7c7e81bb18d337f8bfd5e6d12803
DmitryDatsko <dmitrydatsko.of@gmail.com>
2022-11-04 15:19:44 +0200

    first commit

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using LanguageFeatures.Models;
+using System.Collections;
+
+namespace LanguageFeatures.Views
+{
+    public class ShoppingCart : IProductSelection
+    {
+        private List<Product> products = new();
+        public ShoppingCart(params Product[] prods)
+        {
+            products.AddRange(prods);
+        }
+        public IEnumerable<Product>? Products { get => products; }
+    }
+}
